30557ad9d232684f2f16dc90fcfe6e6932218016
Arai <wataaya1023@gmail.com>
2018-12-01 14:03:41 +0900

    Initial Commit

---
+++ Tategaki/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tategaki
+//
+//  Created by Shiromu on 2018/12/01.
+//  Copyright © 2018 Shiromu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
